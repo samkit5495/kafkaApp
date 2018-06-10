b448db1b67362f13ec3a6e8089965181b270a98b
Samkit Jain <samkit5495@gmail.com>
2018-06-10 17:46:26 +0530

    updates in mongodb models

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,8 +2,16 @@ from mongoengine import *
 from datetime import datetime
 
 
+class User(Document):
+    user_id = IntField(required=True)
+    balance = FloatField(required=True)
+    avg_transaction_amount = FloatField(required=True)
+    stdev = FloatField(required=True)
+    avg_monthly_balance = FloatField(required=True)
+
+    
 class Transaction(Document):
-    user = IntField(required=True)
+    user = ReferenceField(User,required=True)
     date = DateTimeField(required=True)
     amount = FloatField(required=True)
     alert = BooleanField(default=False)
